7a11583d0d31c1c27a32bc57311e76ab52dfac2b
Nikita Yutanov <nikitayutanov@gmail.com>
2023-05-31 18:42:24 +0300

    Prepare template

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
+import { HexString } from '@polkadot/util/types';
+
 const ADDRESS = {
-  NODE: process.env.REACT_APP_NODE_ADDRESS as string,
+  NODE: 'wss://testnet.vara.rs',
+  CONTRACT: '0xf1906fc48a657b95d00b7df50605911898b061b3902bf54a805f3afd6fd17379' as HexString,
 };
 
 const LOCAL_STORAGE = {
